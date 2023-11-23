262351097428917cedc866b3c97eab0ad336a205
SonitaMorency <122633604+SonitaMorency@users.noreply.github.com>
2023-11-23 15:50:16 +0100

    Update RecetteController.js
    correction

--- controllers/RecetteController.js
+++ controllers/RecetteController.js
@@ -1,12 +1,1 @@
-// Appel du modèle
-const Recette = require('../chemin/vers/votre/model/Recette.js');
 
-// Afficher toutes les recettes
-const getRecettes = (req, res) => {
-    Recette.find()
-        .then(recettes => res.status(200).json(recettes))
-        .catch(err => res.status(404).json({ notFound: 'Recette non trouvée' }));
-};
-
-// Export des fonctions
-module.exports = { getRecettes };
