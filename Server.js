49f6ba9095db6c88195c40f9a0ff5bab84b5d3e3
Sonita-M <sonitamorency@gmail.com>
2023-11-23 17:13:17 +0100

    add CRUD for recipe

--- Server.js
+++ Server.js
@@ -4,24 +4,24 @@ const cors = require('cors');
 const app = express();
 const port = 9000;
 const connect = require('./database/Connection');
-const swaggerUi = require('swagger-ui-express');
-const swaggerConf = require('./swaggerConfig');
+/*const swaggerUi = require('swagger-ui-express');
+const swaggerConf = require('./swaggerConfig');*/
  
 // Middleware
 app.use(bodyParse.json());
 app.use(cors());
 app.use(express.json());
  
-const routesPlat = require('./routes/PlatRoutes');
+//const routesPlat = require('./routes/PlatRoutes');
 const routesRecette = require('./routes//RecetteRoutes');
-const routesUser = require('./routes/UserRoutes');
+//const routesUser = require('./routes/UserRoutes');
  
 connect()
  
-app.use('/plats', routesPlat);
+//app.use('/plats', routesPlat);
 app.use('/recettes', routesRecette);
-app.use('/users', routesUser);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConf));
+//app.use('/users', routesUser);
+//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConf));
  
 app.listen(port, () => {
     console.log(`Serveur à l'écoute sur le port ${port}`);
--- routes/RecetteRoutes.js
+++ routes/RecetteRoutes.js
@@ -1,7 +1,19 @@
-
+const recetteController = require('../controllers/RecetteController.js');
 const router = require('express').Router()
 
-//route pour afficher toutes les recettes
-router.get('/getRecette', );
+// Afficher toutes les recettes
+router.get('/', recetteController.getRecettes);
+
+// Afficher une recette en fonction de l'id
+router.get('/recettes/:id', recetteController.getRecetteById);
+
+// Créer une nouvelle recette
+router.post('/recettes', recetteController.createRecette);
+
+// Modifier une recette en fonction de l'id
+router.put('/recettes/:id', recetteController.updateRecette);
+
+// Supprimer une recette en fonction de l'id
+router.delete('/recettes/:id', recetteController.deleteRecette);
 
 module.exports = router;
