657a202ca13725b250f07d77eff4f66518b5bae0
Sonita-M <sonitamorency@gmail.com>
2023-11-23 15:22:10 +0100

    add dishes and users data + add image in plat.js

---
+++ Server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParse = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 9000;
+const connect = require('./database/Connection');
+const swaggerUi = require('swagger-ui-express');
+const swaggerConf = require('./swaggerConfig');
+ 
+// Middleware
+app.use(bodyParse.json());
+app.use(cors());
+app.use(express.json());
+ 
+const routesPlat = require('./routes/PlatRoutes');
+const routesRecette = require('./routes//RecetteRoutes');
+const routesUser = require('./routes/UserRoutes');
+ 
+connect()
+ 
+app.use('/plats', routesPlat);
+app.use('/recettes', routesRecette);
+app.use('/users', routesUser);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConf));
+ 
+app.listen(port, () => {
+    console.log(`Serveur à l'écoute sur le port ${port}`);
+});
---
+++ routes/RecetteRoutes.js
@@ -0,0 +1,7 @@
+
+const router = require('express').Router()
+
+//route pour afficher toutes les recettes
+router.get('/getRecette', );
+
+module.exports = router;
